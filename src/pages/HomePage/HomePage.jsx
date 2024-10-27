43024fb0e41a2c8de048baf33e30b48f217cae9c
Ghalem Salim <salimghalem40@gmail.com>
2024-10-27 21:12:09 +0100

    "Updated CSS styles for ContactUs and HomeDepartments components, and refactored JSX code in HomePage component."

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,26 +1,21 @@
-import React from 'react';
-
-/* import HomeWelcome from '@/components/HomeWelcome/HomeWelcome';
+import HomeWelcome from '@/components/HomeWelcome/HomeWelcome';
 import HomeDepartments from '@/components/HomeDepartments/HomeDepartments';
-import AboutUsTextImage from '@/components/AboutUsTextImage/AboutUsTextImage'; */
-
 import AboutUsTextImage from '@/components/AboutUsTextImage/AboutUsTextImage';
 import WhyJoinUs from '@/components/WhyJoinUs/WhyJoinUs';
 import FAQ from '@/components/FAQ/FAQ';
 import ContactUs from '@/components/ContactUs/ContactUs';
 
-
 const HomePage = () => {
-  return <div>
-
-        
-       
-        <AboutUsTextImage/>
-        <WhyJoinUs/>
-        <ContactUs/>
-        <FAQ/>
-       
-  </div>;
+  return (
+    <div>
+      <HomeWelcome />
+      <HomeDepartments />
+      <AboutUsTextImage />
+      <WhyJoinUs />
+      <ContactUs />
+      <FAQ />
+    </div>
+  );
 };
 
 export default HomePage;
