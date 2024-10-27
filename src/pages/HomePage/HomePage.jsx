e45ef1b8f06e6b4deac718ef4aec69aad64b7c0c
hocineMK03 <hocine182003@gmail.com>
2024-10-27 19:51:20 +0100

    small css fixes

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
 
-import HomeWelcome from '@/components/HomeWelcome/HomeWelcome';
+/* import HomeWelcome from '@/components/HomeWelcome/HomeWelcome';
 import HomeDepartments from '@/components/HomeDepartments/HomeDepartments';
+import AboutUsTextImage from '@/components/AboutUsTextImage/AboutUsTextImage'; */
+
 import AboutUsTextImage from '@/components/AboutUsTextImage/AboutUsTextImage';
-import HomeWhyJoining from '@/components/HomeWhyJoining/HomeWhyJoining';
-import HomeFAQs from '@/components/HomeFAQs/HomeFAQs';
+import WhyJoinUs from '@/components/WhyJoinUs/WhyJoinUs';
+import FAQ from '@/components/FAQ/FAQ';
 import ContactUs from '@/components/ContactUs/ContactUs';
-import StillHaveAQuestion from '@/components/StillHaveAQuestion/StillHaveAQuestion';
+
 
 const HomePage = () => {
   return <div>
 
-        <HomeWelcome/>
-        <HomeDepartments/>
+        
+       
         <AboutUsTextImage/>
-        <HomeWhyJoining/>
+        <WhyJoinUs/>
         <ContactUs/>
-        <HomeFAQs/>
-        <StillHaveAQuestion />
+        <FAQ/>
+       
   </div>;
 };
 
